e964798438b9794934a695bfc99673495670e628
Yun Chul Chang <yunchulchang27@gmail.com>
2021-05-01 19:00:20 -0700

    Improved UI with bootstrap and css

--- job_finder/settings.py
+++ job_finder/settings.py
@@ -132,7 +132,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles"),]
